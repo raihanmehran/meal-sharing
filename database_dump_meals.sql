9bed6c194f9483cf0bad1341d4ad5ff93049564a
Raihan Mehran <raihan.mehran@hotmail.com>
2023-06-05 17:33:05 +0200

    Database script modified

--- database_dump_meals.sql
+++ database_dump_meals.sql
@@ -1,23 +1,30 @@
-CREATE DATABASE  IF NOT EXISTS `meal-sharing`;
-USE `meal-sharing`;
---
--- Table structure for table `meals`
---
+-- Create the database
+CREATE DATABASE IF NOT EXISTS meals;
 
-DROP TABLE IF EXISTS `meals`;
-CREATE TABLE `meals` (
-  `idmeals` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `number_of_guests` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idmeals`)
-) ENGINE=InnoDB AUTO_INCREMENT=6;
+-- Switch to the database
+USE meals;
 
---
--- Dumping data for table `meals`
---
+DROP TABLE IF EXISTS meals;
 
-LOCK TABLES `meals` WRITE;
-/*!40000 ALTER TABLE `meals` DISABLE KEYS */;
-INSERT INTO `meals` VALUES (1,'bla bla bla',3),(2,'bla bla bla',3),(3,'asdasd',7),(4,'benjamins karry',10),(5,'oooooooooo',1);
-/*!40000 ALTER TABLE `meals` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Create the "meals" table
+CREATE TABLE IF NOT EXISTS meals (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255),
+  date DATE
+);
+
+-- Insert 10 meals into the "meals" table
+INSERT INTO meals (title, date) VALUES
+  ('Meal 1', '2023-01-01'),
+  ('Meal 2', '2023-01-02'),
+  ('Meal 3', '2023-01-03'),
+  ('Meal 4', '2023-01-04'),
+  ('Meal 5', '2023-01-05'),
+  ('Meal 6', '2023-01-06'),
+  ('Meal 7', '2023-01-07'),
+  ('Meal 8', '2023-01-08'),
+  ('Meal 9', '2023-01-09'),
+  ('Meal 10', '2023-06-11'),
+  ('Meal 11', '2023-06-12'),
+  ('Meal 12', '2023-06-13'),
+  ('Meal 13', '2023-06-14');
