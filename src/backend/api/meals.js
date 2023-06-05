86e0bf14352e309c88d1a2ad4dc1d3aabf87608a
Raihan Mehran <raihan.mehran@hotmail.com>
2023-06-05 17:41:36 +0200

    database schema refactored to new schema

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -5,7 +5,7 @@ const knex = require("../database");
 router.get("/", async (request, response) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
+    const titles = await knex("meal").select("title");
     console.log(titles);
     response.json(titles);
   } catch (error) {
@@ -17,7 +17,9 @@ router.get("/", async (request, response) => {
 router.get("/future-meals", async (req, res) => {
   try {
     console.log("hello");
-    const row = await knex.raw("SELECT * FROM meals WHERE `date` > now()");
+    const row = await knex.raw(
+      "SELECT * FROM meal WHERE `when_datetime` > now()"
+    );
     res.status(200).json(row[0]);
   } catch (e) {
     res.status(500).json(e);
@@ -27,7 +29,9 @@ router.get("/future-meals", async (req, res) => {
 //Respond with all meals in the past (relative to the when datetime)
 router.get("/past-meals", async (req, res) => {
   try {
-    const row = await knex.raw("SELECT * FROM meals WHERE `date` < now()");
+    const row = await knex.raw(
+      "SELECT * FROM meal WHERE `when_datetime` < now()"
+    );
     res.status(200).json(row[0]);
   } catch (e) {
     res.status(500).json(e);
@@ -37,8 +41,8 @@ router.get("/past-meals", async (req, res) => {
 //Respond with all meals sorted by ID
 router.get("/all-meals", async (req, res) => {
   try {
-    const meals = await knex.raw("SELECT * FROM meals ORDER BY ID");
-    if (meals) res.status(200).json(meals[0]);    
+    const meals = await knex.raw("SELECT * FROM meal ORDER BY ID");
+    if (meals) res.status(200).json(meals[0]);
     res.status(404).json("No meals found!");
   } catch (e) {
     res.status(500).json(e);
@@ -47,7 +51,7 @@ router.get("/all-meals", async (req, res) => {
 //Respond with the first meal (meaning with the minimum id)
 router.get("/first-meal", async (req, res) => {
   try {
-    const meal = await knex.raw("SELECT * FROM meals ORDER BY id ASC limit 1");
+    const meal = await knex.raw("SELECT * FROM meal ORDER BY id ASC limit 1");
     if (meal) {
       res.status(200).json(meal[0][0]);
     } else {
@@ -61,7 +65,7 @@ router.get("/first-meal", async (req, res) => {
 // 	Respond with the last meal (meaning with the maximum id)
 router.get("/last-meal", async (req, res) => {
   try {
-    const meal = await knex.raw("SELECT * FROM meals ORDER BY id desc limit 1");
+    const meal = await knex.raw("SELECT * FROM meal ORDER BY id desc limit 1");
     if (meal) {
       res.status(200).json(meal[0][0]);
     } else {
