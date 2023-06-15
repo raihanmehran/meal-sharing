438632ada29ca59f600a76e87d5a2250ea75e883
Raihan Mehran <raihan.mehran@hotmail.com>
2023-06-15 02:01:52 +0200

    HAMOUDI: Code Reviewed

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -3,6 +3,83 @@ const router = express.Router();
 const knex = require("../database");
 const moment = require("moment");
 
+// http://localhost:5000/api/meals	GET,	Returns all meals
+router.get("/", async (req, res) => {
+  try {
+    console.log("hi");
+    const meals = await knex("meal").select("id", "title");
+
+    meals.length === 0
+      ? res.status(404).send("Meals not found")
+      : res.status(200).send(meals);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// /api/meals	POST, Adds a new meal to the database
+router.post("/", async (req, res) => {
+  try {
+    const meal = req.body;
+
+    const addMeal = await knex("meal").insert(meal);
+    console.log(addMeal.status);
+    console.log(addMeal);
+    res.status(201).send({ message: "Added meal" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// /api/meals/:id	GET,	Returns the meal by id
+router.get("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    const meal = await knex("meal").select("id", "title").where({ id });
+
+    meal.length === 0
+      ? res.status(404).send("Meal not found")
+      : res.status(200).send(meal);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// /api/meals/:id PUT,	Updates the meal by id
+router.put("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const meal = req.body;
+
+    const meals = await knex("meal").select("id");
+    console.log(meals);
+    const updateMeal = await knex("meal").where({ id }).update(meal);
+
+    id > meals.length
+      ? res.status(404).send("Meal not found")
+      : res.status(200).send({ message: "Updated meal" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// /api/meals/:id	DELETE,	Deletes the meal by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    const meals = await knex("meal").select("id");
+    const deleteMeal = await knex("meal").where({ id }).del();
+
+    id > meals.length
+      ? res.status(404).send("Meal not found")
+      : res.status(200).send({ message: "Deleted meal" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.get("/", async (request, response) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
@@ -14,140 +91,60 @@ router.get("/", async (request, response) => {
   }
 });
 
-// Respond with all meals in the future (relative to the when datetime)
-router.get("/future-meals", async (req, res) => {
-  try {
-    console.log("hello");
-    const row = await knex.raw(
-      "SELECT * FROM meal WHERE `when_datetime` > now()"
-    );
-    res.status(200).json(row[0]);
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-//Respond with all meals in the past (relative to the when datetime)
-router.get("/past-meals", async (req, res) => {
-  try {
-    const row = await knex.raw(
-      "SELECT * FROM meal WHERE `when_datetime` < now()"
-    );
-    res.status(200).json(row[0]);
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-//Respond with all meals sorted by ID
-router.get("/all-meals", async (req, res) => {
-  try {
-    const meals = await knex.raw("SELECT * FROM meal ORDER BY ID");
-    if (meals) res.status(200).json(meals[0]);
-    res.status(404).json("No meals found!");
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-//Respond with the first meal (meaning with the minimum id)
-router.get("/first-meal", async (req, res) => {
-  try {
-    const meal = await knex.raw("SELECT * FROM meal ORDER BY id ASC limit 1");
-    if (meal) {
-      res.status(200).json(meal[0][0]);
-    } else {
-      res.status(404).json("there are no meals");
-    }
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-router.get("/:id", async (req, res) => {
-  try {
-    const meal = await knex.raw(
-      `SELECT * FROM meal WHERE ID = ${req.params.id}`
-    );
-    if (meal) {
-      res.status(200).json(meal[0][0]);
-    } else {
-      res.status(404).json("there are no meals");
-    }
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-// First Post Method
-router.post("/add", async (req, res) => {
-  try {
-    const meal = {
-      title: req.body.title,
-      description: req.body.description,
-      location: req.body.location,
-      price: req.body.price,
-      max_reservations: parseInt(req.body.max_reservations),
-      when_datetime: moment().format("YYYY-MM-DD"),
-      created_date: moment().format("YYYY-MM-DD"),
-    };
-    console.log(meal);
-    let query = `INSERT INTO meal (title, description, location, max_reservations, price) VALUES ('${meal.title}', '${meal.description}', '${meal.location}',${meal.max_reservations}, ${meal.price} )`;
-
-    await knex.raw(query);
-
-    console.log("User added");
-
-    res.status(200).json("Hi");
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-router.put("/update", async (req, res) => {
-  try {
-    const meal = {
-      id: parseInt(req.body.id),
-      title: req.body.title,
-      description: req.body.description,
-      location: req.body.location,
-      price: req.body.price,
-      max_reservations: req.body.max_reservations,
-    };
-
-    const isExist = await knex.raw("SELECT * FROM meal WHERE id =" + meal.id);
-    if (isExist[0][0]) {
-      console.log(isExist[0][0]);
-      res.status(200).json("Updated");
-    } else {
-      res.status(400).json("Bad request: No meal found with id :" + meal.id);
-    }
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-router.delete("/delete/:id", async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    if (!id) return res.status(400).json("id not provied");
-
-    const isExist = await knex.raw(
-      "SELECT * FROM meal WHERE id =" + req.params.id
-    );
-    if (isExist[0][0]) {
-      // write code for delettion of the meal
-      res.status(200).json("Deleted");
-    } else {
-      res.status(400).json("Bad request: No meal found with id :" + meal.id);
-    }
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
-
-// 	Respond with the last meal (meaning with the maximum id)
-// router.get("/last-meal", async (req, res) => {
+// // Respond with all meals in the future (relative to the when datetime)
+// router.get("/future-meals", async (req, res) => {
 //   try {
-//     const meal = await knex.raw("SELECT * FROM meal ORDER BY id desc limit 1");
+//     console.log("hello");
+//     const row = await knex.raw(
+//       "SELECT * FROM meal WHERE `when_datetime` > now()"
+//     );
+//     res.status(200).json(row[0]);
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+
+// //Respond with all meals in the past (relative to the when datetime)
+// router.get("/past-meals", async (req, res) => {
+//   try {
+//     console.log("here");
+//     const row = await knex.raw(
+//       "SELECT * FROM meal WHERE `when_datetime` < now()"
+//     );
+//     res.status(200).json(row[0]);
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+
+// //Respond with all meals sorted by ID
+// router.get("/all-meals", async (req, res) => {
+//   try {
+//     const meals = await knex.raw("SELECT * FROM meal ORDER BY ID");
+//     if (meals) res.status(200).json(meals[0]);
+//     res.status(404).json("No meals found!");
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+// //Respond with the first meal (meaning with the minimum id)
+// router.get("/first-meal", async (req, res) => {
+//   try {
+//     const meal = await knex.raw("SELECT * FROM meal ORDER BY id ASC limit 1");
+//     if (meal) {
+//       res.status(200).json(meal[0][0]);
+//     } else {
+//       res.status(404).json("there are no meals");
+//     }
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const meal = await knex.raw(
+//       `SELECT * FROM meal WHERE ID = ${req.params.id}`
+//     );
 //     if (meal) {
 //       res.status(200).json(meal[0][0]);
 //     } else {
@@ -158,28 +155,109 @@ router.delete("/delete/:id", async (req, res) => {
 //   }
 // });
 
-router.get(
-  "/last-meal",
-  getMeals("SELECT * FROM meal ORDER BY id DESC LIMIT 1", 404)
-);
+// // First Post Method
+// router.post("/add", async (req, res) => {
+//   try {
+//     const meal = {
+//       title: req.body.title,
+//       description: req.body.description,
+//       location: req.body.location,
+//       price: req.body.price,
+//       max_reservations: parseInt(req.body.max_reservations),
+//       when_datetime: moment().format("YYYY-MM-DD"),
+//       created_date: moment().format("YYYY-MM-DD"),
+//     };
+//     console.log(meal);
+//     let query = `INSERT INTO meal (title, description, location, max_reservations, price) VALUES ('${meal.title}', '${meal.description}', '${meal.location}',${meal.max_reservations}, ${meal.price} )`;
 
-function getMeals(query, status = 200) {
-  return async (req, res) => {
-    try {
-      const meals = await knex.raw(query);
-      if (meals.length > 1 && meals[0].length > 0) {
-        res.status(200).json(meals[0]);
-      } else {
-        if (status === 200) {
-          res.status(200).json("");
-        } else if (status === 404) {
-          res.status(404).json("There are no meals");
-        }
-      }
-    } catch (e) {
-      res.status(500).json(e.message);
-    }
-  };
-}
+//     await knex.raw(query);
+
+//     console.log("User added");
+
+//     res.status(200).json("Hi");
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+
+// router.put("/update", async (req, res) => {
+//   try {
+//     const meal = {
+//       id: parseInt(req.body.id),
+//       title: req.body.title,
+//       description: req.body.description,
+//       location: req.body.location,
+//       price: req.body.price,
+//       max_reservations: req.body.max_reservations,
+//     };
+
+//     const isExist = await knex.raw("SELECT * FROM meal WHERE id =" + meal.id);
+//     if (isExist[0][0]) {
+//       console.log(isExist[0][0]);
+//       res.status(200).json("Updated");
+//     } else {
+//       res.status(400).json("Bad request: No meal found with id :" + meal.id);
+//     }
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+
+// router.delete("/delete/:id", async (req, res) => {
+//   try {
+//     const id = parseInt(req.params.id);
+//     if (!id) return res.status(400).json("id not provied");
+
+//     const isExist = await knex.raw(
+//       "SELECT * FROM meal WHERE id =" + req.params.id
+//     );
+//     if (isExist[0][0]) {
+//       // write code for delettion of the meal
+//       res.status(200).json("Deleted");
+//     } else {
+//       res.status(400).json("Bad request: No meal found with id :" + meal.id);
+//     }
+//   } catch (e) {
+//     res.status(500).json(e);
+//   }
+// });
+
+// // 	Respond with the last meal (meaning with the maximum id)
+// // router.get("/last-meal", async (req, res) => {
+// //   try {
+// //     const meal = await knex.raw("SELECT * FROM meal ORDER BY id desc limit 1");
+// //     if (meal) {
+// //       res.status(200).json(meal[0][0]);
+// //     } else {
+// //       res.status(404).json("there are no meals");
+// //     }
+// //   } catch (e) {
+// //     res.status(500).json(e);
+// //   }
+// // });
+
+// router.get(
+//   "/last-meal",
+//   getMeals("SELECT * FROM meal ORDER BY id DESC LIMIT 1", 404)
+// );
+
+// function getMeals(query, status = 200) {
+//   return async (req, res) => {
+//     try {
+//       const meals = await knex.raw(query);
+//       if (meals.length > 1 && meals[0].length > 0) {
+//         res.status(200).json(meals[0]);
+//       } else {
+//         if (status === 200) {
+//           res.status(200).json("");
+//         } else if (status === 404) {
+//           res.status(404).json("There are no meals");
+//         }
+//       }
+//     } catch (e) {
+//       res.status(500).json(e.message);
+//     }
+//   };
+// }
 
 module.exports = router;
--- src/backend/app.js
+++ src/backend/app.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const logger = require("./middlewares/logger");
 
+const documents = require("./documents.json");
+const { log } = require("console");
+
 // For week4 no need to look into this!
 // Serve the built client html
 app.use(express.static(buildPath));
@@ -29,6 +32,168 @@ router.use("/meals", mealsRouter);
 app.get("/my-route", async (req, res) => {
   res.send("Hi friend");
 });
+
+// WEEK1 warm ups: HAMOUDI
+app.get("/search", (req, res) => {
+  try {
+    const query = req.query.q;
+    if (!query) {
+      res.status(200).json(documents);
+    } else if (!isNaN(query)) {
+      const result = documents.filter((e) => e.id === query);
+      console.log("s");
+      result.length === 0
+        ? res.status(200).json(documents)
+        : res.status(200).json(result);
+    } else {
+      const result = documents.filter((e) =>
+        Object.values(e).some(
+          (v) =>
+            typeof v === "string" &&
+            v.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+      result.length === 0
+        ? res.status(200).json(documents)
+        : res.status(200).json(result);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.get("/documents/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = documents.filter((e) => e.id === +id);
+    result.length === 0
+      ? res.status(404).json("no result with this id")
+      : res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+app.post("/search", (req, res) => {
+  try {
+    const query = req.query.q;
+    const fields = req.body.fields;
+    console.log("hi");
+    if (!query && !fields) {
+      res.status(200).json(documents);
+      console.log("1");
+    } else if (!query && fields) {
+      console.log("2");
+      const result = documents.filter((e) =>
+        Object.entries(fields).every(([key, value]) => e[key] === value)
+      );
+      result.length === 0
+        ? res.status(404).json("no result with this id")
+        : res.status(200).json(result);
+    } else if (query && !fields) {
+      if (!isNaN(+query)) {
+        const result = documents.filter((e) => e.id === +query);
+        result.length === 0
+          ? res.status(200).json("no result found")
+          : res.status(200).json(result);
+      }
+      console.log("3");
+    } else if (query && fields) {
+      console.log("4");
+      res.status(400).json("Bad request");
+    } else {
+      console.log("5");
+      const result = documents.filter((e) =>
+        Object.values(e).some(
+          (v) =>
+            typeof v === "string" &&
+            v.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+      result.length > 0
+        ? res.status(200).json("no result found")
+        : res.status(200).json(result);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// WEEK2 warmup: NELIA
+// app.get("/search", async (req, res) => {
+//   try {
+//     // const query = req.query.q;
+//     // if (!query) res.status(200).json(documents);
+//     // else {
+//     //   const filteredDocuments = documents.filter((doc) => {
+//     //     if (doc.value) doc.value.includes(query);
+//     //     console.log(doc);
+//     //   });
+//     //   console.log(filteredDocuments);
+//     //   res.status(200).json(filteredDocuments);
+//     // }
+
+//     const query = req.query.q;
+
+//     if (!query) {
+//       res.status(200).json(documents);
+//     } else {
+//       const result = documents.filter((obj) =>
+//         Object.values(obj).some((value) =>
+//           String(value).toLowerCase().includes(query.toLowerCase())
+//         )
+//       );
+
+//       result.length === 0 ? res.sendStatus(404) : res.status(200).json(result);
+//     }
+//   } catch (e) {
+//     res.status(500).json("crashed: " + e);
+//   }
+// });
+
+// // GET /documents/:id
+// app.get("/documents/:id", (req, res) => {
+//   try {
+//     const id = req.params.id;
+
+//     const result = documents.find((obj) => String(obj.id) === id);
+
+//     result ? res.status(200).json(result) : res.sendStatus(404);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// });
+
+// // POST /search
+// app.post("/search", (req, res) => {
+//   try {
+//     const query = req.query.q;
+//     const fields = req.body.fields;
+
+//     if (query && fields) {
+//       res
+//         .status(400)
+//         .json(
+//           "400 Bad request. Both query parameter 'q' in the URL and 'fields' in the request body cannot be provided."
+//         );
+//       return;
+//     } else if (query) {
+//       const result = documents.filter((obj) =>
+//         Object.values(obj).some((value) =>
+//           String(value).toLowerCase().includes(query.toLowerCase())
+//         )
+//       );
+
+//       result.length === 0 ? res.sendStatus(404) : res.status(200).json(result);
+//     } else if (fields) {
+//       const result = documents.filter((obj) =>
+//         Object.keys(fields).every((key) => fields[key] === obj[key])
+//       );
+
+//       result.length === 0 ? res.sendStatus(404) : res.status(200).json(result);
+//     }
+//   } catch (error) {
+//     res.send(500).json(error);
+//   }
+// });
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
